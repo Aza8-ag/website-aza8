b712b0cffda2dc73ab35aea0bd374b24c232e23c
Victor Nascimento <victor.nascimento@aza8.com.br>
2024-08-08 19:26:35 -0300

    add Vue3Marquee to brands

---
+++ components/Brands/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="brands">
+        <ClientOnly>
+            <Vue3Marquee :duration="40">
+                <a class="brands__item" :href="brand.link" target="_blank" v-for="brand in brands" :key="brands.id">
+                    <img :src="brand.url" :alt="brand.name">
+                </a>
+            </Vue3Marquee>
+        </ClientOnly>
+    </div>
+</template>
+
+<script>
+import { Vue3Marquee } from 'vue3-marquee'
+
+export default {
+  components: {
+    Vue3Marquee,
+  },
+  data () {
+        return {
+            brands: [
+                { name: 'Café 3 Corações', url: '/brands/logo-3-coracoes.svg', link: 'https://www.cafe3coracoes.com.br/' },
+                { name: 'Authentic Beauty Concept', url: '/brands/logo-abc.svg', link: 'https://www.clubedocabelopro.com.br/' },
+                { name: 'Artec Revestimentos', url: '/brands/logo-artec.svg', link: 'https://artecrevestimentos.com.br/' },
+                { name: 'Banco Digi+', url: '/brands/logo-digimais.svg', link: 'https://www.bancodigimais.com.br/' },
+                { name: 'Henkel', url: '/brands/logo-henkel.svg', link: 'https://www.henkel.com/' },
+                { name: 'KeyPay - O melhor ecossistema de pagamentos!', url: '/brands/logo-keypay.svg', link: 'https://www.keypay.com.br/' },
+                { name: 'Kawahara Takano | Arquitetura de Negócios', url: '/brands/logo-kt.svg', link: 'https://kt.com.br/' },
+                { name: 'Latem - A casa do mestre', url: '/brands/logo-latem.svg', link: 'https://www.lojaslatem.com.br/' },
+                { name: 'Malwee: Moda feminina, masculina, plus size e infantil', url: '/brands/logo-malwee.svg', link: 'https://www.malwee.com.br/' },
+                { name: 'Pamcary – Seguros Pamcary para você e sua empresa', url: '/brands/logo-pamcary.svg', link: 'https://www1.gps-pamcary.com.br/' },
+                { name: 'Ri Happy Brinquedos - Quanto mais Brincadeira, Melhor!', url: '/brands/logo-rihappy.svg', link: 'https://www.rihappy.com.br/' },
+                { name: 'Schwarzkopf Professional', url: '/brands/logo-schwarzkopf.svg', link: 'https://www.clubedocabelopro.com.br/' },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.brands {
+    @apply w-full max-w-3xl;
+    &__item {
+        @apply mx-1 p-3;
+
+        img {
+            @apply w-full h-full max-w-[85px] max-h-[60px];
+        }
+    }
+}
+</style>
--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,16 +1,8 @@
 <template>
     <footer class="footer">
         <div class="container flex flex-col md:flex-row justify-center items-center md:justify-between">
-            <!-- brands -->
-            <div class="flex items-center justify-start max-w-[350px] sm:max-w-[600px] lg:max-w-[850px] flex-wrap lg:-ml-10">
-
-                <div v-for="brand in brands" :key="brand.id" class="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/6 flex items-center justify-center my-4">
-                    <a :href="brand.link" target="_blank">
-                        <img class="w-full h-full max-w-[85px] max-h-[60px]" :src="brand.url" :alt="brand.name" :title="brand.name">
-                    </a>
-                </div>
-
-            </div>
+            <!-- brands component -->
+            <Brands />
 
             <div class="flex items-center mt-24 md:mt-0 justify-between">
                 <div class="flex items-center">
@@ -35,25 +27,12 @@
 </template>
 
 <script>
+import Brands from '@/components/Brands/index.vue'
+
 export default {
-    data () {
-        return {
-            brands: [
-                { name: 'Café 3 Corações', url: '/brands/logo-3-coracoes.svg', link: 'https://www.cafe3coracoes.com.br/' },
-                { name: 'Authentic Beauty Concept', url: '/brands/logo-abc.svg', link: 'https://www.clubedocabelopro.com.br/' },
-                { name: 'Artec Revestimentos', url: '/brands/logo-artec.svg', link: 'https://artecrevestimentos.com.br/' },
-                { name: 'Banco Digi+', url: '/brands/logo-digimais.svg', link: 'https://www.bancodigimais.com.br/' },
-                { name: 'Henkel', url: '/brands/logo-henkel.svg', link: 'https://www.henkel.com/' },
-                { name: 'KeyPay - O melhor ecossistema de pagamentos!', url: '/brands/logo-keypay.svg', link: 'https://www.keypay.com.br/' },
-                { name: 'Kawahara Takano | Arquitetura de Negócios', url: '/brands/logo-kt.svg', link: 'https://kt.com.br/' },
-                { name: 'Latem - A casa do mestre', url: '/brands/logo-latem.svg', link: 'https://www.lojaslatem.com.br/' },
-                { name: 'Malwee: Moda feminina, masculina, plus size e infantil', url: '/brands/logo-malwee.svg', link: 'https://www.malwee.com.br/' },
-                { name: 'Pamcary – Seguros Pamcary para você e sua empresa', url: '/brands/logo-pamcary.svg', link: 'https://www1.gps-pamcary.com.br/' },
-                { name: 'Ri Happy Brinquedos - Quanto mais Brincadeira, Melhor!', url: '/brands/logo-rihappy.svg', link: 'https://www.rihappy.com.br/' },
-                { name: 'Schwarzkopf Professional', url: '/brands/logo-schwarzkopf.svg', link: 'https://www.clubedocabelopro.com.br/' },
-            ]
-        }
-    }
+  components: {
+    Brands,
+  }
 }
 </script>
 
