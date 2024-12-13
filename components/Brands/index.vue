dd560bc87863eed842f420374b709f82b2378df0
Victor Nascimento <victor.nascimento.ns@gmail.com>
2024-12-13 15:26:35 -0300

    update content

--- components/Brands/index.vue
+++ components/Brands/index.vue
@@ -20,25 +20,26 @@ export default {
   data () {
         return {
             brands: [
-                { name: 'Café 3 Corações', url: '/brands/logo-3-coracoes.svg', link: 'https://www.cafe3coracoes.com.br/' },
                 { name: 'Authentic Beauty Concept', url: '/brands/logo-abc.svg', link: 'https://www.clubedocabelopro.com.br/' },
-                { name: 'Artec Revestimentos', url: '/brands/logo-artec.svg', link: 'https://artecrevestimentos.com.br/' },
                 { name: 'Banco Digi+', url: '/brands/logo-digimais.svg', link: 'https://www.bancodigimais.com.br/' },
-                { name: 'Henkel', url: '/brands/logo-henkel.svg', link: 'https://www.henkel.com/' },
-                { name: 'KeyPay - O melhor ecossistema de pagamentos!', url: '/brands/logo-keypay.svg', link: 'https://www.keypay.com.br/' },
-                { name: 'Kawahara Takano | Arquitetura de Negócios', url: '/brands/logo-kt.svg', link: 'https://kt.com.br/' },
-                { name: 'Latem - A casa do mestre', url: '/brands/logo-latem.svg', link: 'https://www.lojaslatem.com.br/' },
-                { name: 'Malwee: Moda feminina, masculina, plus size e infantil', url: '/brands/logo-malwee.svg', link: 'https://www.malwee.com.br/' },
-                { name: 'Pamcary – Seguros Pamcary para você e sua empresa', url: '/brands/logo-pamcary.svg', link: 'https://www1.gps-pamcary.com.br/' },
-                { name: 'Ri Happy Brinquedos - Quanto mais Brincadeira, Melhor!', url: '/brands/logo-rihappy.svg', link: 'https://www.rihappy.com.br/' },
+                { name: 'Café 3 Corações', url: '/brands/logo-3-coracoes.svg', link: 'https://www.cafe3coracoes.com.br/' },
                 { name: 'Schwarzkopf Professional', url: '/brands/logo-schwarzkopf.svg', link: 'https://www.clubedocabelopro.com.br/' },
+                { name: 'Henkel', url: '/brands/logo-henkel.svg', link: 'https://www.henkel.com/' },
+                { name: 'Kawahara Takano | Arquitetura de Negócios', url: '/brands/logo-kt.svg', link: 'https://kt.com.br/' },
+                { name: 'Pamcary - Seguros Pamcary para você e sua empresa', url: '/brands/logo-pamcary.svg', link: 'https://www1.gps-pamcary.com.br/' },
+                { name: 'Ri Happy Brinquedos - Quanto mais Brincadeira, Melhor!', url: '/brands/logo-rihappy.svg', link: 'https://www.rihappy.com.br/' },
 
-                { name: '', url: '/brands/logo-combo-logistica.png', link: '' },
-                { name: '', url: '/brands/logo-dia.png', link: '' },
-                { name: '', url: '/brands/logo-galo-portugal.png', link: '' },
-                { name: '', url: '/brands/logo-lexus.png', link: '' },
-                { name: '', url: '/brands/logo-porto.png', link: '' },
-                { name: '', url: '/brands/logo-semorin.png', link: '' },
+                // { name: 'Artec Revestimentos', url: '/brands/logo-artec.svg', link: 'https://artecrevestimentos.com.br/' },
+                // { name: 'KeyPay - O melhor ecossistema de pagamentos!', url: '/brands/logo-keypay.svg', link: 'https://www.keypay.com.br/' },
+                // { name: 'Latem - A casa do mestre', url: '/brands/logo-latem.svg', link: 'https://www.lojaslatem.com.br/' },
+                // { name: 'Malwee: Moda feminina, masculina, plus size e infantil', url: '/brands/logo-malwee.svg', link: 'https://www.malwee.com.br/' },
+
+                // { name: '', url: '/brands/logo-combo-logistica.png', link: '' },
+                // { name: '', url: '/brands/logo-dia.png', link: '' },
+                // { name: '', url: '/brands/logo-galo-portugal.png', link: '' },
+                // { name: '', url: '/brands/logo-lexus.png', link: '' },
+                // { name: '', url: '/brands/logo-porto.png', link: '' },
+                // { name: '', url: '/brands/logo-semorin.png', link: '' },
             ]
         }
     }
